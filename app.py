393da457fc7d90511481f2e4654863b4f046a3c0
Vio Azuhri <125689177+viiazuh@users.noreply.github.com>
2026-05-01 23:49:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ elif menu == "Data Management":
 elif menu == "Sentiment Prediction":
     st.markdown("<h2>Sentiment Prediction</h2>", unsafe_allow_html=True)
     with st.container(border=True):
-        input_text = st.text_area("Masukkan teks ulasan", height=150)
+        input_text = st.text_area("Masukkan teks ulasan", placeholder="Contoh: Keren banget!", height=150)
         if st.button("Analisis Sekarang"):
             if input_text.strip():
                 res, emo, conf = get_prediction(input_text)
