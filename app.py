01c6b47d71f024665a7e45e293929b021e16d73f
Azuzu <vioazuhri22@gmail.com>
2026-06-16 21:29:11 +0700

    update peneysuaian bahsa

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ if menu == "Dashboard":
     with c3: st.markdown(f'<div class="metric-card"><div class="metric-title">Negatif 😞</div><div class="metric-value">{s["negatif"]}</div></div>', unsafe_allow_html=True)
     with c4: st.markdown(f'<div class="metric-card"><div class="metric-title">Netral 😐</div><div class="metric-value">{s["netral"]}</div></div>', unsafe_allow_html=True)
     
-    st.markdown("<div style='font-size:16px; font-weight:600; margin-bottom:10px; margin-top:10px;'>Grafik Sentimen (Real-time Session)</div>", unsafe_allow_html=True)
+    st.markdown("<div style='font-size:16px; font-weight:600; margin-bottom:10px; margin-top:10px;'>Aktivitas Terbaru</div>", unsafe_allow_html=True)
     if s["total"] > 0:
         df_rt = pd.DataFrame({
             "Sentimen": ["Positif", "Negatif", "Netral"],
@@ -384,10 +384,10 @@ if menu == "Dashboard":
         with b3: st.markdown(f'<div class="metric-card"><div class="metric-title">Negatif 😞</div><div class="metric-value">{batch_neg}</div></div>', unsafe_allow_html=True)
         with b4: st.markdown(f'<div class="metric-card"><div class="metric-title">Netral 😐</div><div class="metric-value">{batch_net}</div></div>', unsafe_allow_html=True)
         
-        st.markdown("<div style='font-size:16px; font-weight:600; margin-bottom:10px; margin-top:10px;'>Aktivitas Terbaru</div>", unsafe_allow_html=True)
+        st.markdown("<div style='font-size:16px; font-weight:600; margin-bottom:10px; margin-top:10px;'>Grafik Sentimen File Upload</div>", unsafe_allow_html=True)
         df_batch_chart = pd.DataFrame({
             "Sentimen": ["Positif", "Negatif", "Netral"],
-            "Jumlah Data": [batch_pos, batch_neg, batch_net]    
+            "Jumlah Data": [batch_pos, batch_neg, batch_net]
         })
         
         col_bar_batch, col_pie_batch = st.columns(2)
